002f5a30a0024cbe64d46d79bf9bccf8f2136734
rafael <rmfukuda@hs.ind.br>
2023-12-15 10:40:06 -0300

    feat: save data inside sqlite

--- python/main.py
+++ python/main.py
@@ -1,6 +1,50 @@
 import asyncio
+import sqlite3
+import struct
+from datetime import datetime
 
 from bleak import BleakClient
+
+
+class SqlData:
+    def __init__(self) -> None:
+        filename = "sensor.db"
+        self.con = sqlite3.connect(filename)
+        self.cursor = self.con.cursor()
+        self.cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS SensorData (
+                entry_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                sensor REAL,
+                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+            )
+        """
+        )
+
+    def insert(self, reading_value=25.5):
+        # Insert sample sensor data
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        self.cursor.execute(
+            """
+            INSERT INTO SensorData (sensor, timestamp)
+            VALUES (?, ?)
+        """,
+            (reading_value, timestamp),
+        )
+        self.con.commit()
+
+    def select(self):
+        # Read and print sensor data
+        self.cursor.execute("SELECT * FROM SensorData")
+        sensor_data = self.cursor.fetchall()
+
+        print("Sensor Data:")
+        for entry in sensor_data:
+            print("EntryID:", entry[0])
+            print("ReadingValue:", entry[1])
+            print("Timestamp:", entry[2])
+            print()
 
 
 class BleData:
@@ -10,6 +54,7 @@ class BleData:
         self.client = BleakClient(address)
         self.sensor_data = []
         self.duration = duration
+        self.sql = SqlData()
 
     async def main(self):
         try:
@@ -26,7 +71,10 @@ class BleData:
             await self.client.disconnect()
 
     def notify_callback(self, characteristic_handle, sensor_data):
-        self.sensor_data.append(sensor_data)
+        # convert bytearray into float
+        float_value = struct.unpack("<f", sensor_data)[0]
+        self.sensor_data.append(float_value)
+        self.sql.insert(float_value)
 
 
 async def main(duration):
@@ -36,5 +84,9 @@ async def main(duration):
 
 
 if __name__ == "__main__":
-    duration = 10
+    duration = 5
     asyncio.run(main(duration))
+
+    # database read
+    sql_obj = SqlData()
+    sql_obj.select()
